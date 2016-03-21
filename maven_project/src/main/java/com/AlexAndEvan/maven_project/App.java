e64b04a3aecadde589e99206575bbee7b6dd470c
Alex And Evan <gangzhao.forcerealty@gmail.com>
2016-03-20 22:09:39 -0700

    Add Guice library

--- maven_project/src/main/java/com/AlexAndEvan/maven_project/App.java
+++ maven_project/src/main/java/com/AlexAndEvan/maven_project/App.java
@@ -1,5 +1,8 @@
 package com.AlexAndEvan.maven_project;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        Injector injector = Guice.createInjector();
+        System.out.println(injector);
         System.out.println( "Hello World!" );
     }
 }
